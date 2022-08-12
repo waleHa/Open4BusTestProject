42acf9bd7691aa13e0f3e75198518cd8e0d03933
Alwaleed Hamam <alwaleedh@avetti.com>
2022-08-12 07:56:31 -0400

    Connected the Entered Amount Using Ref in ProductItemForm.js and passed ref to allow to read entered value

--- src/Components/Products/ProductItem/ProductItemForm.js
+++ src/Components/Products/ProductItem/ProductItemForm.js
@@ -1,13 +1,33 @@
 import classes from "./ProductItemForm.module.css";
 import Input from "../../UI/Input";
-
+import { useRef, useState } from "react";
 const ProductItemForm = (props) => {
+  //Used to view the error input
+  const [isAmountValid, setIsAmountValid] = useState(true);
+  //Connected the Entered Amount Using Ref in ProductItemForm.js
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    //value: is always string number so + make it number
+    const enteredAmount = +amountInputRef.current.value;
+
+    if (
+      amountInputRef.current.value.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 100
+    ) {
+      setIsAmountValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmount);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
-          id: "amount_"+props.id,
+          id: "amount_" + props.id,
           type: "number",
           min: 1,
           max: 100,
@@ -16,6 +36,7 @@ const ProductItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!isAmountValid && <p>Please, enter a valid amount (1-100)!</p>}
     </form>
   );
 };
